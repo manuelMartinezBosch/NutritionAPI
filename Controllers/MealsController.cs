2bcb207a99d60e73ca4fbc29d03ebab3462093b1
Manuel <manuelmmb1@gmail.com>
2018-09-23 22:26:49 +0200

    clear template

---
+++ Controllers/MealsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MealsController : ControllerBase
+    {
+        /// <summary>
+        /// Get Aliments
+        /// </summary>
+        /// <param name="mealId"></param>
+        /// <returns></returns>
+        [HttpGet("{mealId}", Name = "GetAliments")]
+        public async Task<ActionResult<string>> GetAliments(int mealId)
+        {
+            return "value";
+        }
+
+        // // POST api/values
+        // [HttpPost]
+        // public void Post([FromBody] string value)
+        // {
+        // }
+
+        // // PUT api/values/5
+        // [HttpPut("{id}")]
+        // public void Put(int id, [FromBody] string value)
+        // {
+        // }
+
+        // // DELETE api/values/5
+        // [HttpDelete("{id}")]
+        // public void Delete(int id)
+        // {
+        // }
+    }
+}
